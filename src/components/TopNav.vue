b8516395971d28316fa2d86d11313e3d35b6fcd3
Desmond Murphy <murp0428@algonquinlive.com>
2025-12-02 15:02:46 -0500

    changed top bar

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #333;
+  background-color: #a00000;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
